a9832f8fc66dd40650095e2a3ac70dfce53cbcde
khaledeng77 <hsn793864@gmail.com>
2026-04-13 05:09:13 +0300

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import random
+
+# تحميل البيانات
+#باستخدام مكتبة بانداس نستطيع قراءة ملفات ايكسل ونضعها في جداول 
+users = pd.read_excel("data/users.xlsx")
+products = pd.read_excel("data/products.xlsx")
+ratings = pd.read_excel("data/ratings.xlsx")
+behavior = pd.read_excel("data/behavior.xlsx")
+
+# دمج البيانات
+#نقوم بدمج جدولين السلوكيات والتقييم بناء على رقم المستخدم ورقم المنتج المشترك
+# fillna ونحولها الى صفر بعدها باتخدام nan حتى التي لاتحتوي على تقييم وتضيف التقييم بقيمة behavior فائدتها تحافظ على جميع الصفوف في جدول how 
+data = behavior.merge(ratings, on=["user_id", "product_id"], how="left")
+data = data.fillna(0)
+
+# إعدادات الخوارزمية
+POP_SIZE = 10 #عدد الحلول                 
+GENS = 5   #عدد الاجيال
+REC_SIZE = 5 #عدد المنتجات في كل توصية
+
+#انشاء قائمة بارقام المنتجات ونضعها في متغير
+product_ids = products['product_id'].tolist()
+
+# إنشاء حل عشوائي يتكون منتجات بعدد محدد
+
+def create_individual():
+    return random.sample(product_ids, REC_SIZE)# rec_size  اختيار ارقام عشوائية من قائمة ارقام المنتجات واختيار عددها نسبة ل
+
+# pop_size إنشاء مجموعة حلول نسبة لعدد الحلول المطلوبة
+def create_population():
+    return [create_individual() for _ in range(POP_SIZE)]
+
+# دالة التقييم
+def fitness(individual, user_id):
+    score = 0
+    for pid in individual:
+        # individual نقوم بالبحث في جدول داتا عن الصف الذي يحوي على رقم السمتخدم المطلوب والمنتج الذي يطابق المنتج المراد من التوصية
+        rows = data[(data['user_id'] == user_id) & (data['product_id'] == pid)]
+        if not rows.empty:
+            # اختيار الصف كامل ووضعه في متغير قاموس للوصول الى كل خانه مطلوبة وتقييمها
+            r = rows.iloc[0]
+            score += r['clicked'] * 2
+            score += r['purchased'] * 5
+            score += r['rating']          
+    return score
+
+# اختيار الأفضل
+def selection(population, user_id):
+    # ترتيب مجموعة الحلول بناء على تقييمها في تابع التقييم من الاكبر للاصغر
+    return sorted(population, key=lambda x: fitness(x, user_id), reverse=True)[:POP_SIZE // 2]
+
+# التزاوج
+# نقوم باستخدام توصييتين اباء بتوليد توصية جديدة لاضافتها لمجموعة الحلول
+def crossover(p1, p2):
+    cut = len(p1) // 2 # رقم صحيح يساوي نصف توصية الاب
+    child = p1[:cut] + p2[cut:] #توليد التوصية الجديدة بناء على الرقم الصحيح السابق 
+    return list(set(child))[:REC_SIZE]
+
+# الطفرة
+def mutation(individual):
+    if random.random() < 0.3: # شرط يقوم بتوليد رقم بين صفر وواحد 
+        # اختيار منتج من توصية واستبداله بمنتج معين بشكل عشوائي
+        individual[random.randint(0, len(individual)-1)] = random.choice(product_ids)
+    return individual
+
+# تشغيل الخوارزمية
+def run_ga(user_id):
+    #انشاء مجموعة من التوصيات
+    population = create_population()
+
+    for _ in range(GENS):
+        # تقييم التوصيات واختيار الافضل ووضعها في متغير وحفظها في متغيير التوصيات الجديدة
+        selected = selection(population, user_id)
+        new_population = selected.copy()
+
+        while len(new_population) < POP_SIZE:
+            # توليد اباء جديدة من التوصيات الافضل
+            p1, p2 = random.sample(selected, 2)
+            # دمجها وانشاء توصية جديدة ابن
+            child = crossover(p1, p2)
+            # احداث طفرة في التوصية الابن واضافتها للتوصيات المقيمة
+            child = mutation(child)
+            new_population.append(child)
+
+        population = new_population
+    # باستخدام تابع التقييم score اختيار التوصية الافضل بناء على
+    best = max(population, key=lambda x: fitness(x, user_id))
+    return best
+
+
+# تجربة مباشرة (اختياري)
+if __name__ == "__main__":
+    user_id = users.iloc[0]['user_id']
+    print("Best recommendations:", run_ga(user_id))
---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from algorithm import run_ga, products
+import os
+#انشاء تطبيق الويب وتعريفه على مكان المشروع للوصل الى كافة الملفات
+app = Flask(__name__)
+
+# الى معلومات المنتج IDs تحويل 
+def get_product_info(pid):
+    # المطلوب بمعلوماته الكاملة للمنتج id اختيار المنتج المطابق لل
+    row = products[products['product_id'] == pid].iloc[0]
+    # مثلا Html وارجاعها كقاموس لتسهيل الوصول اليها وارسالها الى 
+    return {        
+        "id": pid,
+        "category": row['category'],
+        "price": row['price']
+    }
+
+# تقوم بربط الرابط المعطى بالدالة المطلوب تشغيلها
+@app.route("/")
+# الدالة الرئيسية
+def home():
+    #تحديد المستخدم المراد
+    user_id = 1
+    #تخزين التوصيات الافضل المعطاة من الخوارزمية داخل متغير
+    rec_ids = run_ga(user_id)
+    # تحويل رقم المنتج الى مواصفاته المطلوبة وتخزينها كقاموس داخل متغير
+    recs = [get_product_info(pid) for pid in rec_ids]
+    # لعرضها Html ارسال جميع هذه البيانات الى صفحة 
+    return render_template("index.html", recs=recs)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",PORT=int(os.environ.get("PORT",5000)))
+
+# app.run(debug=True)
